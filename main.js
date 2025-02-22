e56316c642b6af31f1d514146ac60abca81ec020
Lucho <luiscch@gmail.com>
2025-02-21 22:02:37 -0300

    commit adcional por cambios en main con error para probar debug

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-const http = require('http');
+// Declaración de variables
+let a = 5;
+let b = 10;
 
-console.log('Hola, Mundo!');
+// Suma de las variables
+let resultado = a - b; // Error intencional: operación incorrecta
+
+// Mostrar el resultado en consola
+console.log("El resultado de la suma es: " + resultado);
